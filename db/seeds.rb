5b277976591e9027997fdaa5785eb1bf85bea638
Guilherme Freitas <guilhermeharfy@gmail.com>
2025-08-22 16:23:52 -0400

    data: adding seeds for Menu and MenuItems

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+menu1 = Menu.create!(name: "Lunch")
+menu2 = Menu.create!(name: "Dinner")
+
+menu1.menu_items.create!(name: "Burger", price: 9.00)
+menu1.menu_items.create!(name: "Small Salad", price: 5.00)
+
+menu1.menu_items.create!(name: "Burger", price: 15.00)
+menu1.menu_items.create!(name: "Large Salad", price: 8.00)
